64205e46c775749b8e4574a4bfb4f4deb85d8f9e
i5ting <i5ting@126.com>
2016-06-08 23:50:09 +0800

    lazycommmit, please ignore

--- app.js
+++ app.js
@@ -3,18 +3,17 @@
 require('./init')
 require('./db')
 
-const path = require('path');
+const path = require('path')
 const log4js = require('koa-log4')
 const api = require('koa.res.api')
 const mountRoutes = require('mount-koa-routes')
 const Koa = require('koa')
 const router = require('koa-router')()
 const views = require('koa-views')
-const convert = require('koa-convert')
 const json = require('koa-json')
 const bodyparser = require('koa-bodyparser')()
 const logger = require('koa-logger')
-const serve = require('koa-static');
+const serve = require('koa-static')
 
 const app = new Koa()
 
@@ -22,7 +21,7 @@ const app = new Koa()
 app.use(bodyparser)
 app.use(json())
 app.use(logger())
-app.use(serve(path.join(__dirname, 'public')));
+app.use(serve(path.join(__dirname, 'public')))
 
 log4js.configure('config/log4js.json', { reloadSecs: 300 })
 app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
@@ -52,7 +51,7 @@ app.use(router.routes(), router.allowedMethods())
 // response
 app.on('error', function(err, ctx){
   console.log(err)
-  logger.error('server error', err, ctx);
-});
+  logger.error('server error', err, ctx)
+})
 
 module.exports = app
