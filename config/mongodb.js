43ea41682aa224b85375c98b4a0ddd560de76988
i5ting <i5ting@126.com>
2016-06-08 23:57:23 +0800

    refact, please ignore

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 module.exports = {
-	"host": "127.0.0.1",
-    "port": "27017",
-    "db": "moa-api",
-    "is_debug":true
-};
+  'host': '127.0.0.1',
+  'port': '27017',
+  'db': 'moa-api',
+  'is_debug': true
+}
