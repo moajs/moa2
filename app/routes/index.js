4a2dc6317521b612ce987fbc6059e03046010230
i5ting <i5ting@126.com>
2016-06-08 21:05:40 +0800

    refact

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ var $middlewares  = require('mount-middlewares')(__dirname);
 router.get('/',  (ctx, next) => {
   console.log("../")
   return ctx.render('index', { 
-    title: '欢迎使用Moajs-Api' 
+    title: '欢迎使用，Moa 2 = Moajs with koa 2.x' 
   });
 })
 
