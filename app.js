50c62448f88f57d81afd8daca015abb5ac4f9a01
i5ting <i5ting@126.com>
2016-06-08 23:49:04 +0800

    lazycommmit, please ignore

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
-"use strict";
+'use strict'
 
 require('./init')
 require('./db')
 
-var log4js        = require('koa-log4'); 
-var log           = log4js.getLogger("moa-api");
-var res_api       = require('koa.res.api');
+const path = require('path');
+const log4js = require('koa-log4')
+const api = require('koa.res.api')
+const mountRoutes = require('mount-koa-routes')
+const Koa = require('koa')
+const router = require('koa-router')()
+const views = require('koa-views')
+const convert = require('koa-convert')
+const json = require('koa-json')
+const bodyparser = require('koa-bodyparser')()
+const logger = require('koa-logger')
+const serve = require('koa-static');
 
-const mount_routes = require('mount-koa-routes');
-
-const Koa = require('koa');
-const app = new Koa();
-const router = require('koa-router')();
-const views = require('koa-views');
-const co = require('co');
-const convert = require('koa-convert');
-const json = require('koa-json');
-const onerror = require('koa-onerror');
-const bodyparser = require('koa-bodyparser')();
-const logger = require('koa-logger');
-
+const app = new Koa()
 
 // middlewares
-app.use(convert(bodyparser));
-app.use(convert(json()));
-app.use(convert(logger()));
-app.use(convert(require('koa-static')(__dirname + '/public')));
+app.use(bodyparser)
+app.use(json())
+app.use(logger())
+app.use(serve(path.join(__dirname, 'public')));
 
-log4js.configure('config/log4js.json', { reloadSecs: 300 });
-app.use(log4js.koaLogger(log4js.getLogger("http"), { level: 'auto' }))
+log4js.configure('config/log4js.json', { reloadSecs: 300 })
+app.use(log4js.koaLogger(log4js.getLogger('http'), { level: 'auto' }))
 
-app.use(res_api());
+app.use(api())
 
 app.use(views(__dirname + '/app/views', {
   extension: 'jade'
-}));
+}))
 
 // logger
 app.use((ctx, next) => {
-  console.log("...")
-  const start = new Date();
+  console.log('...')
+  const start = new Date()
   return next().then(() => {
-    const ms = new Date() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-  });
-});
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  })
+})
 
-// router.use('/', index.routes());
-// router.use('/users', users.routes());
-mount_routes(app, __dirname + '/app/routes', true);
+// router.use('/', index.routes())
+// router.use('/users', users.routes())
+mountRoutes(app, __dirname + '/app/routes', true)
 
-app.use(router.routes(), router.allowedMethods());
+app.use(router.routes(), router.allowedMethods())
+
 // response
-
 app.on('error', function(err, ctx){
   console.log(err)
   logger.error('server error', err, ctx);
