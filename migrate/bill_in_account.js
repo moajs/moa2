e384a622ad7933dcb69cb694c2169f34a2f5b0e6
i5ting <i5ting@126.com>
2016-06-09 09:00:57 +0800

    lazycommmit, please ignore

--- migrate/bill_in_account.js
+++ migrate/bill_in_account.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-// 完成发货单，并生成对应账单
-require('../db.js');
- 
-var Test = require('../app/models/user');
+'use strict'
 
-Test.find({}, function(err,docs){
-  console.dir(docs);
-  process.exit();
-});
+// 完成发货单，并生成对应账单
+require('../db.js')
+
+var Test = require('../app/models/user')
+
+Test.find({}, function (err, docs) {
+  if (err) console.log(err)
+  console.dir(docs)
+  process.exit()
+})
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,53 +1,51 @@
 
-function click_del(url){
-  if (!confirm("确认要删除？")) {
-    return window.event.returnValue = false;
+function click_del (url) {
+  if (!window.confirm("确认要删除？")) {
+    return window.event.returnValue = false
   }
   
-  console.log(url);
+  console.log(url)
   
   $.ajax({
     type: "DELETE",
     url: url
   })
-  .done(function( res ) {
-    if(res.status.code == 0){
-      // alert( "Data delete: success " + res.status.msg );
-      window.location.href= location.href;
-    }else{
-      alert( "Data delete fail: " + res.status.msg );
+  .done( function (res) {
+    if (res.status.code == 0) {
+      // alert( "Data delete: success " + res.status.msg )
+      window.location.href= location.href
+    } else {
+      alert( "Data delete fail: " + res.status.msg )
     } 
-  });
+  })
 }
 
-function click_edit(id, url){
-  // $('#' + id).attr('action','#');
-  console.log(url);
+function click_edit (id, url) {
+  // $('#' + id).attr('action','#')
+  console.log(url)
   
   if (!confirm("确认要更新？")) {
-    return window.event.returnValue = false;
+    return window.event.returnValue = false
   }
   
   var form = document.querySelector('form')
-  var data = form2obj(form);
-  console.log(data);
+  var data = form2obj(form)
+  console.log(data)
   
-  // return false;
+  // return false
   $.ajax({
     type: "PATCH",
     url: url,
     data : data
   })
-  .done(function( res ) {
-    if(res.status.code == 0){
-      // alert( "Data delete: success " + res.status.msg );
-      window.location.href= res.data.redirect;
-    }else{
-      alert( "Data delete fail: " + res.status.msg );
+  .done( function( res ) {
+    if (res.status.code == 0){
+      // alert( "Data delete: success " + res.status.msg )
+      window.location.href= res.data.redirect
+    } else {
+      alert( "Data delete fail: " + res.status.msg )
     }
-  });
+  })
   
-  return false;
+  return false
 }
-
-
