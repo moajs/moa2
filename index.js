bd6acc082c43eee303b11d17f39a3112de061dbc
i5ting <i5ting@126.com>
2016-09-09 19:54:25 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const extend = require('extend')
+require('./config/global')
 
+const extend = require('extend')
 
 var conf = {}
 var default_conf = require('./config')
@@ -13,7 +14,7 @@ module.exports = function (conf_files) {
     conf_files = Array.from(arguments)
   }
   
-  console.log('Config files = ' + JSON.stringify(conf_files, null, 4))
+  debug('Config files = ' + JSON.stringify(conf_files, null, 4))
   
   conf_files.map(function (new_conf) {
     conf = extend(conf, require(new_conf))
