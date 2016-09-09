744e2ff1b1ef40132a3e325767642277c8341e23
i5ting <i5ting@126.com>
2016-09-09 19:01:14 +0800

    refact, please ignore

---
+++ examples/helloworld/config/global.js
@@ -0,0 +1,4 @@
+require('log1')(true)
+
+global.Promise = require('bluebird')
+global.debug = require('debug')('moa2');
---
+++ examples/helloworld/config/mongodb.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'host': '127.0.0.1',
+  'port': '27017',
+  'db': 'moa-api',
+  'is_debug': true
+}
